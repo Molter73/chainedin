94244f576815a3f211345d65c8e46a84a84fe749
Mauro Ezequiel Moltrasio <mmoltras@redhat.com>
2024-02-03 12:01:49 +0100

    Limpieza de código

--- js/main.js
+++ js/main.js
@@ -8,28 +8,6 @@ function crearOfertaVacia() {
     return ofertaClone;
 }
 
-// Función para obtener datos simulados del servidor
-function obtenerDatosDelServidor(page) {
-    return new Promise((resolve) => {
-        setTimeout(() => {
-            // Aquí podrías obtener datos reales del servidor
-            var newData = [
-                { titulo: "Oferta 1", descripcion: "Descripción de la oferta 1", precio: "$10" },
-                { titulo: "Oferta 2", descripcion: "Descripción de la oferta 2", precio: "$20" },
-                { titulo: "Oferta 3", descripcion: "Descripción de la oferta 3", precio: "$30" },
-                { titulo: "Oferta 4", descripcion: "Descripción de la oferta 1", precio: "$10" },
-                { titulo: "Oferta 5", descripcion: "Descripción de la oferta 2", precio: "$20" },
-                { titulo: "Oferta 6", descripcion: "Descripción de la oferta 3", precio: "$30" },
-                { titulo: "Oferta 7", descripcion: "Descripción de la oferta 1", precio: "$10" },
-                { titulo: "Oferta 8", descripcion: "Descripción de la oferta 2", precio: "$20" },
-                { titulo: "Oferta 9", descripcion: "Descripción de la oferta 3", precio: "$30" },
-                { titulo: "Oferta 10", descripcion: "Descripción de la oferta 3", precio: "$30" }
-            ];
-            resolve(newData);
-        }, 1000);
-    });
-}
-
 // Función para llenar la lista con ofertas
 async function llenarListaOfertas() {
     const listaOfertas = document.querySelector("#listaOfertas");
