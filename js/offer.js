f3f186954c3b7815b5230af7e13c4839b6a1c78b
Mauro Ezequiel Moltrasio <mmoltras@redhat.com>
2024-02-03 20:13:46 +0100

    Botón de regreso funcionando

--- js/offer.js
+++ js/offer.js
@@ -1,13 +1,20 @@
-document.addEventListener("DOMContentLoaded", async function() {
-    let params = new URLSearchParams(window.location.search);
-    let id = params.get("id");
-
-    let response = await fetch("../php/jobs.php?" + new URLSearchParams({
-        id: id,
-    }))
+async function subscribe() {
+    let body = new FormData();
+    body.set("job_id", id);
+    let response = await fetch("../php/apply.php",{
+        method: "POST",
+        headers: {
+            "Accept": "application/json",
+            "Content-Type": "application/x-www-form-urlencoded",
+        },
+        body: new URLSearchParams({
+            "job_id": id,
+        }),
+    })
         .then((response) => response.json());
-    let data = response.data;
+}
 
+function fill_offer(data) {
     let name = document.getElementById("nombre-empresa");
     name.textContent = data.company;
 
@@ -16,22 +23,24 @@ document.addEventListener("DOMContentLoaded", async function() {
 
     let description = document.getElementById("descripcion");
     description.textContent = data.description;
+}
+
+document.addEventListener("DOMContentLoaded", async function() {
+    let params = new URLSearchParams(window.location.search);
+    let id = params.get("id");
+
+    let response = await fetch("../php/jobs.php?" + new URLSearchParams({
+        id: id,
+    }))
+        .then((response) => response.json());
+
+    fill_offer(response.data);
 
     let subsciption_button = document.getElementById("subscribe");
-    subsciption_button.addEventListener("click", async function() {
-        let body = new FormData();
-        body.set("job_id", id);
-        let response = await fetch("../php/apply.php",{
-            method: "POST",
-            headers: {
-                "Accept": "application/json",
-                "Content-Type": "application/x-www-form-urlencoded",
-            },
-            body: new URLSearchParams({
-                "job_id": id,
-            }),
-        })
-            .then((response) => response.json());
-        console.log(response);
+    subsciption_button.addEventListener("click", subscribe);
+
+    let back_button = document.getElementById("back");
+    back_button.addEventListener("click", function() {
+        window.location.href = "../index.html";
     });
 });
